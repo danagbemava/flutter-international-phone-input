1bc61e068ff21258c903dacd49011966e68eaa1f
danagbemava <52815643+danagbemava@users.noreply.github.com>
2020-11-27 16:18:25 +0000

    update libphonenumber version

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter_s"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Deana\Flutter\flutter_libraries\flutter-international-phone-input\example"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/admin/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/admin/Documents/projects/flutter/open_source/flutter-international-phone-input/example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter_s\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/admin/dev/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
